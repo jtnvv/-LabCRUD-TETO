07254b1e7f9c50a4c0862d82fce9b9bd0224ce12
daperezch <daperezch@unal.edu.co>
2024-04-10 10:55:22 -0500

    crudTrabajo

---
+++ Backend/src/routes/trabajo.js
@@ -0,0 +1,81 @@
+const db = require('../db')
+const { Router } = require('express')
+
+const router = Router()
+
+// CRUD trabajo ////////////////////////////////////
+
+// CREATE 
+router.post("/trabajo", async (req, res) => {
+    try {
+      const { cargo, empresa, salario, id_persona } = req.body;
+      const trabajoTemp = await db.query(
+        "INSERT INTO trabajo (cargo, empresa, salario, id_persona) VALUES($1, $2, $3, $4) RETURNING *",
+        [cargo, empresa, salario, id_persona]
+      );
+  
+      res.json(trabajoTemp.rows[0]);
+    } catch (err) {
+      res.json(err.message);
+      console.error(err.message);
+    }
+  });
+  
+  // READ 
+  router.get("/trabajo", async (req, res) => {
+    try {
+      const allTodos = await db.query("SELECT * FROM trabajo");
+      res.json(allTodos.rows);
+    } catch (err) {
+      res.json(err.message);
+      console.error(err.message);
+    }
+  });
+  
+  // READ INDIVIDUAL
+  router.get("/trabajo/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const todo = await db.query("SELECT * FROM trabajo WHERE id_trabajo = $1",
+        [id]
+      );
+  
+      res.json(todo.rows[0]);
+    } catch (err) {
+      res.json(err.message);
+      console.error(err.message);
+    }
+  });
+  
+  // UPDATE 
+  router.put("/trabajo/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { cargo, empresa, salario, id_persona } = req.body;
+      const updateTodo = await db.query(
+        "UPDATE trabajo SET direccion = $2, capacidad = $3, niveles = $4 WHERE id_trabajo = $1",
+        [cargo, empresa, salario, id_persona]
+      );
+  
+      res.json("datos de trabajo actualizados");
+    } catch (err) {
+      res.json(err.message);
+      console.error(err.message);
+    }
+  });
+  
+  // DELETE
+  router.delete("/trabajo/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const deleteTodo = await db.query("DELETE FROM trabajo WHERE id_trabajo = $1",
+        [id]
+      );
+      res.json("datos de trabajo eliminados");
+    } catch (err) {
+      res.json(err.message);
+      console.log(err.message);
+    }
+  });
+
+  module.exports = router
